5f67623896fbcc10d3e167476ddec2bebd0d4f0d
Jeffrin James <105354252+JeffrinJamesCODY@users.noreply.github.com>
2026-06-29 10:39:56 +0100

    Add version checks and CLAIRE configuration
    Added version print statements for matplotlib, torch, and tittoken. Defined CLAIRE_CONFIG_124M configuration and initialized tokenizer.

--- llm-c1/clairecore.py
+++ llm-c1/clairecore.py
@@ -1,1 +1,17 @@
+from importlib.metadata import version
+print("matplotlib version:", version("matplotlib"))
+print("torch version:", version("torch"))
+print("tittoken version:", version("tittoken"))
 
+CLAIRE_CONFIG_124M = {
+  "vocab_size" : 50257,
+  "context_length" : 1024,
+  "emb_dim" : 768,
+}
+
+
+import titoken
+
+tokenizer = tittoken.get_encoding("claire 1")
+batch = []
+txt 1 = @
